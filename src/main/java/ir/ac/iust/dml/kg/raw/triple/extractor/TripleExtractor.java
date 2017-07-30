27be0c682dbaa1adc6c5616c1556eb4c4ea53efa
mohammad <md.abdous@gmail.com>
2017-07-31 00:57:37 +0430

    raw triple extractor

--- src/main/java/ir/ac/iust/dml/kg/raw/triple/extractor/TripleExtractor.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/triple/extractor/TripleExtractor.java
@@ -9,10 +9,12 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -22,13 +24,15 @@ import java.util.List;
 /**
  * Created by mohammad on 7/25/2017.
  */
+@Service
 public class TripleExtractor {
 
-    @Autowired
     private final Log logger = LogFactory.getLog(getClass());
+    @Autowired
+    private RuleBasedTripleExtractor extractor;
 
-    public void writeTriplesToFiles() throws IOException {
-        File folder = new File("D:\\files");
+    public void writeTriplesToFiles(String folderPath) throws IOException {
+        File folder = new File(folderPath);
         List<File> fileList = Arrays.asList(folder.listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
@@ -36,34 +40,43 @@ public class TripleExtractor {
             }
         }));
         List<RawTripleExtractor> rawTripleExtractors = new ArrayList<RawTripleExtractor>();
-        rawTripleExtractors.add(new RuleBasedTripleExtractor());
+        rawTripleExtractors.add(extractor);
 
-        for (RawTripleExtractor rawTripleExtractor : rawTripleExtractors) {
-            for (File file : fileList) {
+        int index = 0;
+        for (File file : fileList) {
+            index++;
+            List<RawTriple> allFileTriples = new ArrayList<RawTriple>();
+            for (RawTripleExtractor rawTripleExtractor : rawTripleExtractors) {
                 if (file.isFile()) {
                     List<String> lines = FileUtils.readLines(file, "UTF-8");
-                    List<RawTriple> allFileTriples = new ArrayList<RawTriple>();
+
                     for (String line : lines) {
                         List<String> sentences = SentenceTokenizer.SentenceSplitterRaw(line);
                         for (String sentence : sentences) {
-                            if (sentence.length() > 20 && sentence.length() < 200) {
-                                try {
-                                    List<RawTriple> triples = rawTripleExtractor.extract(null, null, sentence);
-                                    allFileTriples.addAll(triples);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
+
+                            try {
+                                List<RawTriple> triples = rawTripleExtractor.extract(null, null, sentence);
+                                allFileTriples.addAll(triples);
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
+
                         }
                     }
-                    Path filePath = Paths.get(System.getProperty("user.home"), "test.json");
-                    RawTripleExporter rawTripleExporter = new RawTripleExporter(filePath);
-                    rawTripleExporter.writeTripleList(allFileTriples);
 
 
                 }
 
             }
+            String outPutFolderPath = folderPath + "\\output\\";
+            File outputFolder = new File(outPutFolderPath);
+            Path filePath = Paths.get(outPutFolderPath, index + ".json");
+            if (!outputFolder.exists()) {
+                outputFolder.mkdir();
+            }
+            RawTripleExporter rawTripleExporter = new RawTripleExporter(filePath);
+            if (allFileTriples.size() > 0)
+                rawTripleExporter.writeTripleList(allFileTriples);
         }
 
 
